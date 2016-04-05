f375dca339d4922519e733a5a67b9185b28b2e2d
Patrick <(prwoodbury@crimson.ua.edu)>
2016-04-05 15:58:24 -0500

    Add initial implementation of UserActionActivity.

--- app/src/main/java/com/cs495/gesconnect/MainActivity.java
+++ app/src/main/java/com/cs495/gesconnect/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int i;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
