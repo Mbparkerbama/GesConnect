cca7dfe99135ed5161068e1af23dfc37a767a21a
Patrick <prwoodbury@crimson.ua.edu>
2016-04-05 15:50:30 -0500

    Add initial implementation of UserActionActivity.

--- app/src/main/java/com/cs495/gesconnect/UserActionActivity.java
+++ app/src/main/java/com/cs495/gesconnect/UserActionActivity.java
@@ -1,21 +1,20 @@
 package com.cs495.gesconnect;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UserActionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_action);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,10 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Intent intent = new Intent(this, UserActionActivity.class);
-        startActivity(intent);
 
-        finish();
     }
 
     @Override
@@ -55,4 +51,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
