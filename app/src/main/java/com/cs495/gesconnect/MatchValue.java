1d8c5d426262e26d0f1fd8a5fa4d112f99143e1b
Matt <Mbparker@crimson.ua.edu>
2016-04-06 14:47:19 -0500

    Commit! (Matt)- MatchValue Skeleton

---
+++ app/src/main/java/com/cs495/gesconnect/MatchValue.java
@@ -0,0 +1,13 @@
+package com.cs495.gesconnect;
+
+public class MatchValue {
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    private float value;
+}
