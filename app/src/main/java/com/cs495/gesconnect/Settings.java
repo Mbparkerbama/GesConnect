d048edb3f603a2d3adb9e9c57d2e64129269be85
Matt <Mbparker@crimson.ua.edu>
2016-04-06 15:07:50 -0500

    Commit! (Matt)- GestureList and Settings skeleton (Nobody reads these)

---
+++ app/src/main/java/com/cs495/gesconnect/Settings.java
@@ -0,0 +1,25 @@
+package com.cs495.gesconnect;
+
+/**
+ * Created by Matt on 4/6/2016.
+ */
+public class Settings {
+    private String key;
+    private GestureList gesturelist;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public GestureList getGesturelist() {
+        return gesturelist;
+    }
+
+    public void setGesturelist(GestureList gesturelist) {
+        this.gesturelist = gesturelist;
+    }
+}
