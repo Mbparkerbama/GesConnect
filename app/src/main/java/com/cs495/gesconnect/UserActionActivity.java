92a5cb4bfb83166f365ff978f374d147a2e66454
Spencer Baer <spencer@spencerbaer.com>
2016-04-06 12:41:58 -0500

    Attach drawing layout to UserActivity.

--- app/src/main/java/com/cs495/gesconnect/UserActionActivity.java
+++ app/src/main/java/com/cs495/gesconnect/UserActionActivity.java
@@ -1,23 +1,68 @@
 package com.cs495.gesconnect;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class UserActionActivity extends AppCompatActivity {
+import com.myscript.atk.sltw.SingleLineWidget;
+import com.myscript.atk.sltw.SingleLineWidgetApi;
+import com.myscript.certificate.MyCertificate;
+
+public class UserActionActivity extends AppCompatActivity implements
+        SingleLineWidgetApi.OnConfiguredListener,
+        SingleLineWidgetApi.OnTextChangedListener
+{
+    private static final String TAG = "UserActionActivity";
+    private SingleLineWidgetApi widget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_action);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.drawing_surface);
+
+        widget = (SingleLineWidget) findViewById(R.id.singleLine_widget);
+        if (!widget.registerCertificate(MyCertificate.getBytes()))
+        {
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("Please use a valid certificate.");
+            dlgAlert.setTitle("Invalid certificate");
+            dlgAlert.setCancelable(false);
+            dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    //dismiss the dialog
+                }
+            });
+            dlgAlert.create().show();
+            return;
+        }
+
+        widget.setOnConfiguredListener(this);
+        widget.setOnTextChangedListener(this);
+        widget.setAutoScrollEnabled(false);
+
+        // references assets directly from the APK to avoid extraction in application
+        // file system
+        widget.addSearchDir("zip://" + getPackageCodePath() + "!/assets/conf");
+
+        // The configuration is an asynchronous operation. Callbacks are provided to
+        // monitor the beginning and end of the configuration process and update the UI
+        // of the input method accordingly.
+        //
+        // "en_US" references the en_US bundle name in conf/en_US.conf file in your assets.
+        // "cur_text" references the configuration name in en_US.conf
+        widget.configure("en_US", "cur_text");
 
 
     }
@@ -42,6 +87,37 @@ public class UserActionActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        widget.setOnTextChangedListener(null);
+        widget.setOnConfiguredListener(null);
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConfigured(SingleLineWidgetApi singleLineWidgetApi, boolean success) {
+        if(!success)
+        {
+            Toast.makeText(getApplicationContext(), widget.getErrorString(), Toast.LENGTH_LONG).show();
+            Log.e(TAG, "Unable to configure the Single Line Widget: " + widget.getErrorString());
+            return;
+        }
+        Toast.makeText(getApplicationContext(), "Single Line Widget Configured", Toast.LENGTH_SHORT).show();
+        if(BuildConfig.DEBUG)
+            Log.d(TAG, "Single Line Widget configured!");
+    }
+
+    @Override
+    public void onTextChanged(SingleLineWidgetApi singleLineWidgetApi, String s, boolean b) {
+        Toast.makeText(getApplicationContext(), "Recognition update", Toast.LENGTH_SHORT).show();
+        if(BuildConfig.DEBUG)
+        {
+            Log.d(TAG, "Single Line Widget recognition: " + widget.getText());
+        }
     }
 
     public void openSettings(View view) {
