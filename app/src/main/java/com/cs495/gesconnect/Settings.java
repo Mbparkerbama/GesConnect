eef77deddf4ae521a2efea5c38e7a4f9961840d5
Patrick <prwoodbury@crimson.ua.edu>
2016-04-06 16:28:28 -0500

    Add basic GestureList storage functions to Settings

--- app/src/main/java/com/cs495/gesconnect/Settings.java
+++ app/src/main/java/com/cs495/gesconnect/Settings.java
@@ -2,6 +2,7 @@ package com.cs495.gesconnect;
 
 import android.content.SharedPreferences;
 import android.content.Context;
+import com.cs495.gesconnect.GestureList;
 
 public class Settings {
     public static String getSetting(Context context, String key) {
@@ -24,14 +25,27 @@ public class Settings {
 
         SharedPreferences.Editor editor
             = sharedPreferences.edit();
-
         editor.putString(key, value);
         editor.commit();
     }
 
-/*    public static GestureList getGestureList() {
+    public static GestureList getGestureList() {
+        return gestureList;
+    }
 
-    } */
+    public static void saveGestureList(Context context) {
+        setSetting(context,
+                gestureListStorageName,
+                gestureList.save());
+    }
+
+    public static void loadGestureList(Context context) {
+        gestureList.load(getSetting(context,
+                                gestureListStorageName,
+                                ""));
+    }
+
+    static private GestureList gestureList;
 
     // String accessors for application settings
     final static public String vibrationEnabledString = "GesVibrationEnabled";
@@ -41,5 +55,7 @@ public class Settings {
     final static public String falseString = "false";
 
     // Name of the shared preference file the key/value pairs are stored to
-    final static protected String preferencesFileName = "GesConnectPref";
+    final static private String preferencesFileName = "GesConnectPref";
+
+    final static private String gestureListStorageName = "GesMasterGestureList";
 }
