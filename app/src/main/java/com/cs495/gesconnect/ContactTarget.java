941a1e11757d94368257f2c08106f3de64258db2
Patrick <prwoodbury@crimson.ua.edu>
2016-04-06 17:12:58 -0500

    Add serialization methods to ContactTarget

--- app/src/main/java/com/cs495/gesconnect/ContactTarget.java
+++ app/src/main/java/com/cs495/gesconnect/ContactTarget.java
@@ -1,5 +1,15 @@
 package com.cs495.gesconnect;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
 public class ContactTarget {
     public String getLookupKey() {
         return lookupKey;
@@ -13,8 +23,37 @@ public class ContactTarget {
         return phoneType;
     }
 
-    public void setPhoneType(int phonteType) {
+    public void setPhoneType(int phoneType) {
         this.phoneType = phoneType;
+    }
+
+    public String save() {
+        try {
+            OutputStream stream = new ByteArrayOutputStream();
+            ObjectOutput output = new ObjectOutputStream(stream);
+            output.writeObject(lookupKey);
+            output.writeObject(phoneType);
+            return output.toString();
+        }
+        catch (Exception e) {
+
+        }
+
+        return "";
+    }
+
+    public void load(String data) {
+        try {
+            InputStream stream = new ByteArrayInputStream(
+                    data.getBytes());
+            ObjectInput input = new ObjectInputStream(stream);
+            lookupKey = (String) input.readObject();
+            phoneType = (int) input.readObject();
+        }
+        catch (Exception e) {
+
+        }
+
     }
 
     private String lookupKey;
